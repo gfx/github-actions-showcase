a9e53b9844a2f5e452c0c407c84ea4af105df11b
FUJI Goro <goro@fastly.com>
2022-05-13 10:55:16 +0900

    use @actions/core

---
+++ tools/summaries.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env npx ts-node --compilerOptions {"moduleResolution":"node"}
+import { summary } from "@actions/core";
+
+summary
+  .addHeading("Hello, world! 🚀")
+  .addRaw(`This is a job summary that runs on ${process.platform}-${process.arch}`)
+  .addEOL()
+  .write()
+  .then(() => {
+    console.log("Summary written.");
+  })
+  .catch((err) => {
+    console.error("Failed to output a job summary!", err);
+  });
